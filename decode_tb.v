dfd63fc2e48661279f07b0151b23d8d9fbd85215
Snehin Kukreja <snehin.kukreja@gmail.com>
2020-06-23 02:04:40 +0200

    implemented a moving average filter
    takes the last 4 samples and averages them (integer division) to smoothen the output

--- decode_tb.v
+++ decode_tb.v
@@ -29,6 +29,9 @@ module decode_tb(
     reg reset;
     reg start;
     reg [4:0] counter;
+    reg signed [7:0] delay2;
+    reg signed [7:0] delay3;
+    wire signed [7:0] avg;
     
     always@(posedge CLK100MHZ) begin
         
@@ -43,12 +46,24 @@ module decode_tb(
         .result(out)
     );
     
+    filter test (
+        .CLK100MHZ(CLK100MHZ),
+        .reset(reset),
+        .start(start),
+        .current(out),
+        .delay(delay),
+        .delay2(delay2),
+        .delay3(delay3),
+        .result(avg)
+    );
+    
     initial begin
         start = 0;
         reset = 1;
-        
         encode <= 8'b11101010;
         delay <= 8'd0;
+        delay2 <= 8'd0;
+        delay3 <= 8'd0;
         counter <= 0;
         #100
         reset = 0;
@@ -56,6 +71,9 @@ module decode_tb(
     end
     
     always@(negedge CLK100MHZ) begin
+        
+        delay3 = delay2;
+        delay2 = delay;
         delay = out;
     end   
     
